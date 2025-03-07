7c5ee2ab3d5a88f1e5cfdf496dc4dd10c606fe65
soominn <chosm0129@naver.com>
2025-03-08 00:22:03 +0900

    ALLOWED_HOSTS 수정

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",") if os.getenv("ALLOWED_HOSTS") else []
 
 
 # Application definition
