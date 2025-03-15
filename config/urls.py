eb41fdc2efd91eacc8358a650f5629c80188a2bd
soominn <chosm0129@naver.com>
2025-03-15 13:33:16 +0900

    운영에 맞춘 urls.py 수정

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('simword/', include('simword.urls')),
+    path('api/admin/', admin.site.urls),
+    path('api/simword/', include('simword.urls')),
 ]
